7fbad9b4f75302430f7424d56a3c09aa8b070889
MTuan247 <mtuan2472000@gmail.com>
2021-10-31 11:54:57 +0700

    codelab 2.1

---
+++ codelab2.1/TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -0,0 +1,48 @@
+package com.example.twoactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    public static final String EXTRA_REPLY = "com.example.twoactivities.extra.REPLY";
+    private EditText mReply;
+    private Button btn_reply;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
+
+        mReply = findViewById(R.id.editText_second);
+        btn_reply = findViewById(R.id.button_second);
+        btn_reply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                returnReply(view);
+            }
+        });
+
+
+    }
+
+    public void returnReply(View view) {
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
+    }
+
+}
